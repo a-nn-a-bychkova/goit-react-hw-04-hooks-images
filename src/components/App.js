bd8558555707dbdf5742ed09aafb5ad11068b002
Anna Bychkova <bychkova.dev@gmail.com>
2021-01-16 18:53:09 +0200

    adds hooks in App and ImageGallery

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Searchbar from './Searchbar';
@@ -6,48 +6,85 @@ import Searchform from './Searchform';
 import ImageGallery from './ImageGallery';
 import Modal from './Modal';
 
-class App extends Component {
-  state = {
-    searchQuery: '',
-    showModal: false,
-    largeImage: { src: '', alt: '' },
+function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [largeImage, setLargeImage] = useState({ src: '', alt: '' });
+
+  const handleFormSubmit = searchQuery => {
+    setSearchQuery(searchQuery);
   };
 
-  handleFormSubmit = searchQuery => {
-    this.setState({ searchQuery });
-  };
-
-  handleImageClick = (src, alt) => {
-    this.setState({ largeImage: { src, alt } });
+  const handleImageClick = (src, alt) => {
+    setLargeImage({ src, alt });
     this.toggleModal();
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  const toggleModal = () => {
+    setShowModal(showModal => !showModal);
   };
-
-  render() {
-    const { searchQuery, largeImage, showModal } = this.state;
-    return (
-      <div>
-        <Searchbar>
-          <Searchform onSubmit={this.handleFormSubmit} />
-        </Searchbar>
-        <ImageGallery
-          searchQuery={searchQuery}
-          onClick={this.handleImageClick}
+  return (
+    <div>
+      <Searchbar>
+        <Searchform onSubmit={handleFormSubmit} />
+      </Searchbar>
+      <ImageGallery searchQuery={searchQuery} onClick={handleImageClick} />
+      {showModal && (
+        <Modal
+          src={largeImage.src}
+          alt={largeImage.alt}
+          onClose={toggleModal}
         />
-        {showModal && (
-          <Modal
-            src={largeImage.src}
-            alt={largeImage.alt}
-            onClose={this.toggleModal}
-          />
-        )}
-        <ToastContainer />
-      </div>
-    );
-  }
+      )}
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
+
+// class App extends Component {
+//   state = {
+//     searchQuery: '',
+//     showModal: false,
+//     largeImage: { src: '', alt: '' },
+//   };
+
+//   handleFormSubmit = searchQuery => {
+//     this.setState({ searchQuery });
+//   };
+
+//   handleImageClick = (src, alt) => {
+//     this.setState({ largeImage: { src, alt } });
+//     this.toggleModal();
+//   };
+
+//   toggleModal = () => {
+//     this.setState(({ showModal }) => ({ showModal: !showModal }));
+//   };
+
+//   render() {
+//     const { searchQuery, largeImage, showModal } = this.state;
+//     return (
+//       <div>
+//         <Searchbar>
+//           <Searchform onSubmit={this.handleFormSubmit} />
+//         </Searchbar>
+//         <ImageGallery
+//           searchQuery={searchQuery}
+//           onClick={this.handleImageClick}
+//         />
+//         {showModal && (
+//           <Modal
+//             src={largeImage.src}
+//             alt={largeImage.alt}
+//             onClose={this.toggleModal}
+//           />
+//         )}
+//         <ToastContainer />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
